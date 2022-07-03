afe52a5f9d45d63bd1da9541f39a8aeefe3f4cdf
BlondeNamazu <namazutitech15@gmail.com>
2022-07-04 00:23:41 +0900

    add checkwords

--- script.kts
+++ script.kts
@@ -1,10 +1,14 @@
 import java.io.File
 import java.security.MessageDigest
 
+val m = 18
+val k = 3
+
 val md5 = MessageDigest.getInstance("MD5")
+val bloom = Array(m){false}
 
 // get index list using hash function
-fun getHashedIndices(k: Int, m: Int, input: String): List<Int> {
+fun getHashedIndices(input: String): List<Int> {
   var target = input
   var result = mutableListOf<Int>()
 
@@ -18,6 +22,9 @@ fun getHashedIndices(k: Int, m: Int, input: String): List<Int> {
       // get byte value in (-128 to 127) and convert to positive range (0 to 255)
       val positiveIndex = digest[i].toInt() + 128
 
+      // finally, index is composed of digest 0 to 6. Index in bit expression looks like below:
+      // |digest 0|digest 1|digest 2|digest 3|...|digest 6|
+      // |01000101|01110101|01010100|11010101|...|00010101|
       index = index * 256 + positiveIndex
 
       // "NMZ" is not contained in dictionary, so it is suit for prefix
@@ -31,27 +38,28 @@ fun getHashedIndices(k: Int, m: Int, input: String): List<Int> {
   return result.toList()
 }
 
-val m = 18
-val k = 3
-
-val bloom = Array(m){false}
+fun isContained(input: String): Boolean {
+  val indices = getHashedIndices(input)
+  return indices.all { index ->
+    bloom[index]
+  }
+}
+  
 
 val dictionary = File("dictionary.txt").readLines()
 
-/*
 // load dictionary and save result to bloom[m] using hashed indices
 for (word in dictionary) {
-  val indices = getHashedIndices(k, m, word)
+  val indices = getHashedIndices(word)
   for (index in indices) {
     bloom[index] = true
   }
 }
-*/
 
 // check words
-// memo: use standard input
+println("print out words both contained in random5-chars")
+val checkWords = File("random5-chars.txt").readLines()
+for (word in checkWords) {
+  if(isContained(word)) println(word)
+}
 
-getHashedIndices(k, m, dictionary[0]).forEach { println(it) }
-getHashedIndices(k, m, dictionary[1]).forEach { println(it) }
-getHashedIndices(k, m, dictionary[2]).forEach { println(it) }
-getHashedIndices(k, m, dictionary[3]).forEach { println(it) }
